142e43c58e34a7f6a5fde4644ce1f35366e5d7a4
Brayan-724 <bjqf0724@gmail.com>
2025-02-09 00:23:58 -0700

    style: `alejandra`

--- flake.nix
+++ flake.nix
@@ -10,26 +10,39 @@
     fenix,
     ...
   }: let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    lib = pkgs.lib;
 
-      fenix-lib = fenix.packages.${system};
+    fenix-lib = fenix.packages.${system};
 
-      rust-toolchain = fenix-lib.fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
-      };
+    rust-toolchain-toml = (lib.importTOML ./rust-toolchain.toml).toolchain;
+    rust-toolchain = fenix-lib.fromToolchainName {
+      name = rust-toolchain-toml.channel;
+      sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+    };
+
+    libraries = with pkgs; [
+      gtk3-x11.dev
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      libxkbcommon
+    ];
   in {
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs;
+        [
           nushell
           pkg-config
           openssl
-          rust-toolchain
-        ];
-      };
+          (rust-toolchain.withComponents (rust-toolchain-toml.components))
+        ]
+        ++ libraries;
+
+      LD_LIBRARY_PATH = lib.makeLibraryPath libraries;
+    };
   };
 }
